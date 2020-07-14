891a051b4808ace544fceff597afee9b37ca2616
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-14 07:21:50 +0200

    Add reducer for app

---
+++ src/reducers/appReducer.ts
@@ -0,0 +1,31 @@
+import { SET_LOADING, SET_NOTIFICATION } from "../constants";
+import { ReducerAction } from "../common/types";
+
+const initialState = {
+  loading: false,
+  notification: {
+    type: null,
+    message: null,
+    hide: true,
+  },
+};
+
+export default function appReducer(
+  state = initialState,
+  action: ReducerAction
+) {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, loading: action.payload };
+    case SET_NOTIFICATION:
+      return {
+        ...state,
+        notification: {
+          ...state.notification,
+          ...action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+}

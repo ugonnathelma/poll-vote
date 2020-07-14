3470a2fb56e84aaa7e79f9a49824b14465dcd7f1
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-14 07:32:10 +0200

    Add function to post new question

---
+++ src/lib/createQuestion.ts
@@ -0,0 +1,16 @@
+type NewQuestionType = { question: string; choices: string[] };
+
+export default async ({ question, choices }: NewQuestionType) => {
+  const response = await fetch(`https://polls.apiblueprint.org/questions?`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      question,
+      choices: choices,
+    }),
+  });
+
+  return response.status !== 201 ? null : await response.json();
+};

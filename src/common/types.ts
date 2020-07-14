0e0914727ae27480951dc7c16fd6417439549d4e
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-14 07:36:43 +0200

    Add type definitions

---
+++ src/common/types.ts
@@ -0,0 +1,37 @@
+export type QuestionType = {
+  question: string;
+  published_at: string;
+  url: string;
+  totalVotes: number;
+  choices: {
+    choice: string;
+    url: string;
+    votes: number;
+  }[];
+};
+
+export type NotificationType = {
+  type: string | null;
+  message: string | null;
+  hide: boolean;
+};
+
+export type DefaultRootState = {
+  appReducer: {
+    loading: boolean;
+    notification: {
+      type: null | string;
+      message: null | string;
+      hide: boolean;
+    };
+  };
+  questionReducer: {
+    questions: QuestionType[];
+    creating: boolean;
+  };
+};
+
+export type ReducerAction = {
+  type: string;
+  payload: any;
+};

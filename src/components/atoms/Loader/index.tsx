d03fa63761d57dedc32417c3227f21de370149b6
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-14 07:34:06 +0200

    Add Loader component

---
+++ src/components/atoms/Loader/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import Image from "../../assets/loader.gif";
+
+const Loader = () => {
+  return <img src={Image} alt={Image} width="100px" />;
+};
+export default Loader;
